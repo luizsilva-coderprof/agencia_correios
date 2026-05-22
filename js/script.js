cff3d0765a87dbc1b499964b27bf19ac7b33c3f4
luizsilva-coderprof <luiz.silva@prof.unipar.br>
2026-05-22 17:51:00 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,41 +1,37 @@
+const botaoenviar = document.getElementById('btn')
+const res = document.getElementById('res')
 
-// --- 1. EVENTOS DOM (Via JavaScript Listeners) ---
-var botao = document.getElementById('btn')
-var res = document.getElementById('res')
-
-botao.addEventListener('click', processar)
-botao.addEventListener('mouseenter', entrou)
-botao.addEventListener('mouseout', saiu)
+botaoenviar.addEventListener('click', processar)
+botaoenviar.addEventListener('mouseenter', entrou)
+botaoenviar.addEventListener('mouseout', saiu)
 
 function entrou() {
-    botao.style.background = 'darkgreen'
+    botaoenviar.style.background = 'yellow'
+    botaoenviar.style.color = 'black'
 }
 
 function saiu() {
-    botao.style.background = 'green'
+    botaoenviar.style.background = 'green'
+    botaoenviar.style.color = 'white'
 }
 
-// --- 2. LÓGICA DO SISTEMA ---
 function processar() {
-    var txtn = document.getElementById('txtnome')
-    var txti = document.getElementById('txtidade')
+    const txtn = document.getElementById('txtnome')
+    const txti = document.getElementById('txtidade')
     
-    var nome = txtn.value
-    var idade = Number(txti.value)
+    let nome = txtn.value
+    let idade = Number(txti.value)
 
-    // Validação simples para não rodar sem dados
     if (nome == '' || txti.value == '') {
-        res.innerHTML = '[ERRO] Preencha todos os campos!'
+        res.innerHTML = '[ERRO] Obrigatório preencher os dados!'
         return 
     }
 
-    // --- 3. MANIPULAÇÃO DE DATAS E HORAS ---
-    var agora = new Date()
-    var hora = agora.getHours()
-    var diaSem = agora.getDay()
+    let agora = new Date()
+    let hora = agora.getHours()
+    let diaSem = agora.getDay()
 
-    // Texto de saudação baseado na hora
-    var saudacao = ''
+    let saudacao = ''
     if (hora < 12) {
         saudacao = 'Bom dia'
     } else if (hora <= 18) {
@@ -44,8 +40,7 @@ function processar() {
         saudacao = 'Boa noite'
     }
 
-    // --- 4. CONDIÇÃO ANINHADA (Prioridade por Idade) ---
-    var classe = ''
+    let classe = ''
     if (idade < 16) {
         classe = 'Atendimento Recusado (Menor de idade desacompanhado)'
     } else if (idade < 60) {
@@ -56,8 +51,7 @@ function processar() {
         classe = 'Fila Prioridade Especial (80+)'
     }
 
-    // --- 5. CONDIÇÃO MÚLTIPLA (Destinação por Dia da Semana) ---
-    var guiche = ''
+    let guiche = ''
     switch(diaSem) {
         case 0:
             guiche = 'Fechado (Domingo)'
@@ -85,7 +79,6 @@ function processar() {
             break
     }
 
-    // --- 6. SAÍDA DE DADOS (Inner HTML + Template String) ---
     res.innerHTML = `<p>${saudacao}, <strong>${nome}</strong>!</p>`
     res.innerHTML += `<p>Sua classificação: <strong>${classe}</strong></p>`
     
